92394ec5a1983eee571a42e578e4c7f62f31fd5f
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-10-21 19:36:04 +0530

    Initial commit

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Anshul Kapoor on 20/10/2017.
+//  Copyright © 2017 Anshul Kapoor. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
